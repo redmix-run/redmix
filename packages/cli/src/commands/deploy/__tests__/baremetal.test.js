3062179539da7a0e3904f45089fbb6d2ab938da0
Tobbe Lundberg <tobbe@tlundberg.com>
2025-04-18 15:57:51 +0200

    chore(cli): Make baremetal safe to import in cli (#29)

--- packages/cli/src/commands/deploy/__tests__/baremetal.test.js
+++ packages/cli/src/commands/deploy/__tests__/baremetal.test.js
@@ -11,7 +11,7 @@ vi.mock('@redmix/project-config', async (importOriginal) => {
   }
 })
 
-import * as baremetal from '../baremetal.js'
+import * as baremetal from '../baremetal/baremetalHandler.js'
 
 describe('verifyConfig', () => {
   it('throws an error if no environment specified', () => {
