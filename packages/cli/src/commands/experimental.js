74342ea0da3227267ac3de30adc8a73650f167d8
Tobbe Lundberg <tobbe@tlundberg.com>
2025-04-18 06:49:23 +0200

    chore(esm): Don't use yargs' .commandDir

--- packages/cli/src/commands/experimental.js
+++ packages/cli/src/commands/experimental.js
@@ -2,23 +2,23 @@ import terminalLink from 'terminal-link'
 
 import detectRwVersion from '../middleware/detectProjectRwVersion.js'
 
+import * as experimentalInngest from './experimental/setupInngest.js'
+import * as experimentalOpenTelemetry from './experimental/setupOpenTelemetry.js'
+import * as experimentalReactCompiler from './experimental/setupReactCompiler.js'
+import * as experimentalRsc from './experimental/setupRsc.js'
+import * as experimentalStreamingSsr from './experimental/setupStreamingSsr.js'
+
 export const command = 'experimental <command>'
 export const aliases = ['exp']
 export const description = 'Run or setup experimental features'
 
 export const builder = (yargs) =>
   yargs
-    .commandDir('./experimental', {
-      recurse: true,
-      // @NOTE This regex will ignore all commands nested more than two
-      // levels deep.
-      // e.g. /setup/hi.js & setup/hi/hi.js are picked up, but
-      // setup/hi/hello/bazinga.js will be ignored
-      // The [/\\] bit is for supporting both windows and unix style paths
-      // Also take care to not trip up on paths that have "setup" earlier
-      // in the path by eagerly matching in the start of the regexp
-      exclude: /.*[/\\]experimental[/\\].*[/\\].*[/\\]/,
-    })
+    .command(experimentalInngest)
+    .command(experimentalOpenTelemetry)
+    .command(experimentalReactCompiler)
+    .command(experimentalRsc)
+    .command(experimentalStreamingSsr)
     .demandCommand()
     .middleware(detectRwVersion)
     .epilogue(
--- packages/cli/src/commands/setup/deploy/deploy.js
+++ packages/cli/src/commands/setup/deploy/deploy.js
@@ -2,9 +2,23 @@ export const command = 'deploy <target>'
 export const description = 'Setup deployment to various targets'
 import terminalLink from 'terminal-link'
 
+import * as setupDeployBaremetal from './providers/baremetal.js'
+import * as setupDeployCoherence from './providers/coherence.js'
+import * as setupDeployFlightcontrol from './providers/flightcontrol.js'
+import * as setupDeployNetlify from './providers/netlify.js'
+import * as setupDeployRender from './providers/render.js'
+import * as setupDeployServerless from './providers/serverless.js'
+import * as setupDeployVercel from './providers/vercel.js'
+
 export const builder = (yargs) =>
   yargs
-    .commandDir('./providers', { recurse: true })
+    .command(setupDeployBaremetal)
+    .command(setupDeployCoherence)
+    .command(setupDeployFlightcontrol)
+    .command(setupDeployNetlify)
+    .command(setupDeployRender)
+    .command(setupDeployServerless)
+    .command(setupDeployVercel)
     .demandCommand()
     .option('force', {
       alias: 'f',
