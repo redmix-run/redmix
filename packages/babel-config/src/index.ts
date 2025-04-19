1034025711006a4c3b746e258ffd0e8a8d3e4be3
Tobbe Lundberg <tobbe@tlundberg.com>
2025-04-19 23:59:38 +0200

    chore(babel-config): Restore comment to point to RedwoodJS

--- packages/babel-config/src/index.ts
+++ packages/babel-config/src/index.ts
@@ -1,6 +1,6 @@
 // The excess of exports here is for backwards compatibility
-// since `@redmix/internal` exported everything from babel/api, web, and common.
-// See https://github.com/redmix-run/redmix/blob/44b4a9023ac3a14b5f56b071052bdf49c410bb8b/packages/internal/src/index.ts#L13-L16.
+// since `@redwoodjs/internal` exported everything from babel/api, web, and common.
+// See https://github.com/redwoodjs/redwood/blob/44b4a9023ac3a14b5f56b071052bdf49c410bb8b/packages/internal/src/index.ts#L13-L16.
 
 export {
   BABEL_PLUGIN_TRANSFORM_RUNTIME_OPTIONS,
