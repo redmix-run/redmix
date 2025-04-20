1b7606662c2ce8bd7bfebecdcf1abda6caf66c9b
Tobbe Lundberg <tobbe@tlundberg.com>
2025-04-21 00:47:38 +0200

    chore(ci): Simplify repro from #32

--- tasks/test-project/codemods/classWithPrivate.ts
+++ tasks/test-project/codemods/classWithPrivate.ts
@@ -1,16 +1,11 @@
 const foo = `class Bar {}
 
 class Foo {
-  // Without the correct babel plugins this will throw an error
-  private b = new Bar()
-
-  bar() {
-    return this.b
-  }
+  public bar = new Bar()
 }
 
 export const ClassWithPrivate = () => {
-  return <p>{new Foo().bar().toString()}</p>
+  return <p>{new Foo().bar.toString()}</p>
 }`
 
 export default () => {
