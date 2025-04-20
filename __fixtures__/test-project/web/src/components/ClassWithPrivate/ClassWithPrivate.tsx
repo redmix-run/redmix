f9094a2e4910755e3664add5888add4d5772e7f2
Tobbe Lundberg <tobbe@tlundberg.com>
2025-04-21 00:18:06 +0200

    fix(app tests): Fix v8 regression with classes (#32)

---
+++ __fixtures__/test-project/web/src/components/ClassWithPrivate/ClassWithPrivate.tsx
@@ -0,0 +1,14 @@
+class Bar {}
+
+class Foo {
+  // Without the correct babel plugins this will throw an error
+  private b = new Bar()
+
+  bar() {
+    return this.b
+  }
+}
+
+export const ClassWithPrivate = () => {
+  return <p>{new Foo().bar().toString()}</p>
+}
--- packages/babel-config/src/web.ts
+++ packages/babel-config/src/web.ts
@@ -10,6 +10,7 @@ import { getConfig, getPaths } from '@redmix/project-config'
 import type { RegisterHookOptions } from './common'
 import {
   CORE_JS_VERSION,
+  getCommonPlugins,
   registerBabel,
   parseTypeScriptConfigFiles,
   getPathsFromTypeScriptConfig,
@@ -17,7 +18,11 @@ import {
 
 // These flags toggle on/off certain features
 export interface Flags {
-  forJest?: boolean // will change the alias for module-resolver plugin
+  /**
+   * Will change the alias for module-resolver plugin and include a few extra
+   * babel plugins
+   */
+  forJest?: boolean // will change the alias for module-resolver plugin and
   forPrerender?: boolean // changes what babel-plugin-redwood-routes-auto-loader does
   forJavaScriptLinting?: boolean // will enable presets to supporting linting in the absence of typescript related presets/plugins/parsers
 }
@@ -44,6 +49,7 @@ export const getWebSideBabelPlugins = (
   const plugins = [
     // It is important that this plugin run first, as noted here: https://react.dev/learn/react-compiler
     useReactCompiler && ['babel-plugin-react-compiler', { target: '19' }],
+    ...(forJest ? getCommonPlugins() : []),
     // === Import path handling
     [
       'babel-plugin-module-resolver',
