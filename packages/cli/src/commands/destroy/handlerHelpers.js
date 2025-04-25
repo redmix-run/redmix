ac63683ca24cdde04ccf1ad9c0a6c8d64e46c683
Tobbe Lundberg <tobbe@tlundberg.com>
2025-04-25 16:19:34 +0200

    chore(cli): destroy cellHandler() (ESM prep)

---
+++ packages/cli/src/commands/destroy/handlerHelpers.js
@@ -0,0 +1,33 @@
+import { Listr } from 'listr2'
+
+import { recordTelemetryAttributes } from '@redmix/cli-helpers'
+
+import { deleteFilesTask } from '../../lib/index.js'
+
+const tasks = ({ componentName, filesFn, name }) =>
+  new Listr(
+    [
+      {
+        title: `Destroying ${componentName} files...`,
+        task: async () => {
+          const f = await filesFn({ name, stories: true, tests: true })
+          return deleteFilesTask(f)
+        },
+      },
+    ],
+    { rendererOptions: { collapseSubtasks: false }, exitOnError: true },
+  )
+
+export function createHandler({
+  componentName,
+  preTasksFn = (options) => options,
+  filesFn,
+}) {
+  return async (options) => {
+    recordTelemetryAttributes({
+      command: `destroy ${componentName}`,
+    })
+    options = await preTasksFn({ ...options, isDestroyer: true })
+    await tasks({ componentName, filesFn, name: options.name }).run()
+  }
+}
