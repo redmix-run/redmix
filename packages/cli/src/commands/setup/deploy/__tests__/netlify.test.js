fd9a4b3388baaaa92c96804d5a1e97e80087b438
Tobbe Lundberg <tobbe@tlundberg.com>
2025-04-28 18:46:16 +0200

    chore(cli/setup): Make setup/deploy ESM compatible (#47)

--- packages/cli/src/commands/setup/deploy/__tests__/netlify.test.js
+++ packages/cli/src/commands/setup/deploy/__tests__/netlify.test.js
@@ -6,10 +6,11 @@ import fs from 'fs-extra'
 import { vol } from 'memfs'
 import { vi, describe, it, expect, beforeEach } from 'vitest'
 
+// Mock telemetry and other things
 import '../../../../lib/test'
+
 import { getPaths } from '../../../../lib/index.js'
 import { updateApiURLTask } from '../helpers/index.js'
-// Mock telemetry and other things
 
 vi.mock('../../../../lib', async (importOriginal) => {
   const { printSetupNotes } = await importOriginal()
@@ -65,7 +66,7 @@ beforeEach(() => {
 
 describe('netlify', () => {
   it('should call the handler without error', async () => {
-    const netlify = await import('../providers/netlify')
+    const netlify = await import('../providers/netlifyHandler')
 
     let error = undefined
     try {
