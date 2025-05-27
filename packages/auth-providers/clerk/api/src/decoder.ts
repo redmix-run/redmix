a5d0ca3308ef5b709770dab558ce35fc920a41e0
Tobbe Lundberg <tobbe@tlundberg.com>
2025-05-27 22:00:52 +0200

    feat(clerk): Switch to `@clerk/backend` SDK

--- packages/auth-providers/clerk/api/src/decoder.ts
+++ packages/auth-providers/clerk/api/src/decoder.ts
@@ -1,44 +1,5 @@
 import type { Decoder } from '@cedarjs/api'
 
-/**
- * @deprecated This function will be removed; it uses a rate-limited API. Use `clerkAuthDecoder` instead.
- */
-export const authDecoder: Decoder = async (token: string, type: string) => {
-  if (type !== 'clerk') {
-    return null
-  }
-
-  const { createClerkClient, verifyToken } = await import(
-    '@clerk/clerk-sdk-node'
-  )
-
-  try {
-    const options = {
-      apiUrl: process.env.CLERK_API_URL || 'https://api.clerk.dev',
-      jwtKey: process.env.CLERK_JWT_KEY,
-      secretKey: process.env.CLERK_SECRET_KEY,
-    }
-
-    const jwtPayload = await verifyToken(token, options)
-
-    if (!jwtPayload.sub) {
-      return Promise.reject(new Error('Session invalid'))
-    }
-
-    const clerkClient = createClerkClient(options)
-
-    const user = await clerkClient.users.getUser(jwtPayload.sub)
-
-    return {
-      ...user,
-      roles: user.publicMetadata['roles'] ?? [],
-    }
-  } catch (error) {
-    console.error(error)
-    return Promise.reject(error)
-  }
-}
-
 export const clerkAuthDecoder: Decoder = async (
   token: string,
   type: string,
@@ -47,11 +8,11 @@ export const clerkAuthDecoder: Decoder = async (
     return null
   }
 
-  const { verifyToken } = await import('@clerk/clerk-sdk-node')
+  const { verifyToken } = await import('@clerk/backend')
 
   try {
     const jwtPayload = await verifyToken(token, {
-      apiUrl: process.env.CLERK_API_URL || 'https://api.clerk.dev',
+      apiUrl: process.env.CLERK_API_URL,
       jwtKey: process.env.CLERK_JWT_KEY,
       secretKey: process.env.CLERK_SECRET_KEY,
     })
