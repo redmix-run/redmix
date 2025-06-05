a816d8576f32ffa0787d2922e98c1488df9cdb9b
Tobbe Lundberg <tobbe@tlundberg.com>
2025-06-05 22:31:39 +0200

    update web/vite.config in create-cedar-app templates

--- packages/create-cedar-app/templates/js/web/vite.config.js
+++ packages/create-cedar-app/templates/js/web/vite.config.js
@@ -8,8 +8,10 @@ import redwood from '@cedarjs/vite'
 // See: https://vitejs.dev/config/server-options.html#server-host.
 dns.setDefaultResultOrder('verbatim')
 
-const viteConfig = {
+export default defineConfig({
   plugins: [redwood()],
-}
-
-export default defineConfig(viteConfig)
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
+})
