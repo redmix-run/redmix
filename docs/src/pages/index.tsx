95c8d066f387675fb445e175713a3679b5c72007
Tobbe Lundberg <tobbe@tlundberg.com>
2025-06-08 09:45:15 +0200

    add content

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,20 +1,80 @@
-import * as React from 'react'
+import React from 'react'
 
-import { Redirect, useLocation } from '@docusaurus/router'
+import Layout from '@theme/Layout'
 
-import config from '../../docusaurus.config'
+export default function Home() {
+  return (
+    <Layout title="CedarJS" description="The React + GraphQL Web App Framework">
+      <div
+        style={{
+          maxWidth: '1024px',
+          margin: '0 auto',
+          padding: '2em',
+        }}
+      >
+        <section style={{ textAlign: 'center' }}>
+          <img
+            src="https://avatars.githubusercontent.com/u/211931789?s=200&v=4"
+            width="200"
+          />
+          <h1 style={{ textAlign: 'center' }}>CedarJS</h1>
+          <p
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              gap: '.5em',
+              justifyContent: 'center',
+            }}
+          >
+            <a href="https://discord.gg/8mNkAgby5m">
+              <img
+                src="https://img.shields.io/badge/Discord-5865F2?style=for-the-badge&logo=discord&logoColor=white"
+                alt="Join our Discord server!"
+              />
+            </a>
+            <a href="https://github.com/cedarjs/cedar">
+              <img
+                src="https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white"
+                alt="GitHub page"
+              />
+            </a>
+            <a href="/docs">
+              <img
+                src="https://img.shields.io/badge/Documentation-3ECC5F?style=for-the-badge&logo=readthedocs&logoColor=white"
+                alt="Documentation"
+              />
+            </a>
+          </p>
+        </section>
+        <h1>About</h1>
 
-const { defaultDocsLandingPage } = config.customFields
+        <p>
+          CedarJS is a fork of the <a href="https://redwoodjs.com">RedwoodJS</a>{' '}
+          framework with the goal of taking it into the modern web development
+          era with a smooth transition path for existing RedwoodJS applications.
+        </p>
 
-function Home() {
-  const location = useLocation()
-  const defaultUrl = [
-    location.pathname.replace(/\/$/, ''),
-    'docs',
-    defaultDocsLandingPage,
-  ].join('/')
+        <p>
+          CedarJS is a reliable, modern, and actively maintained full-stack
+          React framework used in production by both large and small companies.
+        </p>
 
-  return <Redirect to={defaultUrl} />
+        <p>
+          CedarJS would obviously not be where it is today without the vision
+          and heroic efforts of the RedwoodJS founders, maintainers and
+          community.
+        </p>
+
+        <blockquote>
+          cedar has become a powerful symbol of strength and revitalization
+          <cite style={{ display: 'block' }}>
+            —{' '}
+            <a href="https://indigenousfoundations.arts.ubc.ca/cedar/">
+              https://indigenousfoundations.arts.ubc.ca/cedar/
+            </a>
+          </cite>
+        </blockquote>
+      </div>
+    </Layout>
+  )
 }
-
-export default Home
